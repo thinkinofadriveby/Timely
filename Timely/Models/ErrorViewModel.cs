df4f695a99865edb5754e6e5a5489be2ff1d75cb
nazar <nazar@DESKTOP-837JG7F>
2023-03-19 21:35:12 +0200

    Add project files.

---
+++ Timely/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Timely.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
